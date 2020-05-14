8724482ffe5686736161aeb42a04aa25d78b8f63
fionastefanilimin <fiona16ti@mahasiswa.pcr.ac.id>
2020-05-14 10:13:29 +0700

    update files and umur_bb
    adding alert dialog on umur_bb.dart file and running app on ios simulator

--- lib/umur_bb.dart
+++ lib/umur_bb.dart
@@ -1,206 +1,237 @@
 import 'package:flutter/material.dart';
 import 'dart:convert' as JSON;
+
 void main() => runApp(UmurBB());
 String dropdownValue;
 double _value = 0.0;
-int _valueSlide=0;
+int _valueSlide = 0;
 final bbController = TextEditingController();
 final tbController = TextEditingController();
-
-
 
 class UmurBB extends StatefulWidget {
   UmurBB({Key key, this.title}) : super(key: key);
 
   final String title;
-  
+
   @override
   _UmurBBState createState() => _UmurBBState();
 }
+
 var _warna;
+
 class _UmurBBState extends State<UmurBB> {
   String _hasil = "";
-  
-  
-  _rumus_umur_bb(int _x, double _y,String jk) {
-    if(jk=="Laki-laki"){
+  String _keterangan = "";
+
+  _rumus_umur_bb(int _x, double _y, String jk) {
+    if (jk == "Laki-laki") {
       var dataList = [
-      [2.1, 2.5, 3.9],
-      [2.9, 3.4, 5.1],
-      [3.8, 4.3, 6.3],
-      [4.4, 5, 7.2],
-      [4.9, 5.6, 7.8],
-      [5.3, 6, 8.4],
-      [5.7, 6.4, 8.8],
-      [5.9, 6.7, 9.2],
-      [6.2, 6.9, 9.6],
-      [6.4, 7.1, 9.9],
-      [6.6, 7.4, 10.2],
-      [6.8, 7.6, 10.5],
-      [6.9, 7.7, 10.8],
-      [7.1, 7.9, 11],
-      [7.2, 8.1, 11.3],
-      [7.4, 8.3, 11.5],
-      [7.5, 8.4, 11.7],
-      [7.7, 8.6, 12],
-      [7.8, 8.8, 12.2],
-      [8, 8.9, 12.5],
-      [8.1, 9.1, 12.7],
-      [8.2, 9.2, 12.9],
-      [8.4, 9.4, 13.2],
-      [8.5, 9.5, 13.4],
-      [8.6, 9.7, 13.6],
-      [8.8, 9.8, 13.9],
-      [8.9, 10, 14.1],
-      [9, 10.1, 14.3],
-      [9.1, 10.2, 14.5],
-      [9.2, 10.4, 14.8],
-      [9.4, 10.5, 15],
-      [9.5, 10.7, 15.2],
-      [9.6, 10.8, 15.4],
-      [9.7, 10.9, 15.6],
-      [9.8, 11, 15.8],
-      [9.9, 11.2, 16],
-      [10, 11.3, 16.2],
-      [10.1, 11.4, 16.4],
-      [10.2, 11.5, 16.6],
-      [10.3, 11.6, 16.8],
-      [10.4, 11.8, 17],
-      [10.5, 11.9, 17.2],
-      [10.6, 12, 17.4],
-      [10.7, 12.1, 17.6],
-      [10.8, 12.2, 17.8],
-      [10.9, 12.4, 18],
-      [11, 12.5, 18.2],
-      [11.1, 12.6, 18.4],
-      [11.2, 12.7, 18.6],
-      [11.3, 12.8, 18.8],
-      [11.4, 12.9, 19],
-      [11.5, 13.1, 19.2],
-      [11.6, 13.2, 19.4],
-      [11.7, 13.3, 19.6],
-      [11.8, 13.4, 19.8],
-      [11.9, 13.5, 20],
-      [12, 13.6, 20.2],
-      [12.1, 13.7, 20.4],
-      [12.2, 13.8, 20.6],
-      [12.3, 14, 20.8],
-      [12.4, 14.1, 21]
-    ];
-    if (_y < dataList[_x][0]) {
-      setState(() {
-                 _warna=Colors.red;
-                }
-  );
-      _hasil = "Berat badan sangat kurang (severely underweight)";
-    } else if (_y >= dataList[_x][0] && _y < dataList[_x][1]) {
-      setState(() {
-                 _warna=Colors.orangeAccent;
-                }
-  );
-      _hasil = "Berat badan kurang (underweight)";
-    } else if (_y >= dataList[_x][1] && _y < dataList[_x][2]) {
-      setState(() {
-                 _warna=Colors.green;
-                }
-  );
-      _hasil = "Berat badan normal";
+        [2.1, 2.5, 3.9],
+        [2.9, 3.4, 5.1],
+        [3.8, 4.3, 6.3],
+        [4.4, 5, 7.2],
+        [4.9, 5.6, 7.8],
+        [5.3, 6, 8.4],
+        [5.7, 6.4, 8.8],
+        [5.9, 6.7, 9.2],
+        [6.2, 6.9, 9.6],
+        [6.4, 7.1, 9.9],
+        [6.6, 7.4, 10.2],
+        [6.8, 7.6, 10.5],
+        [6.9, 7.7, 10.8],
+        [7.1, 7.9, 11],
+        [7.2, 8.1, 11.3],
+        [7.4, 8.3, 11.5],
+        [7.5, 8.4, 11.7],
+        [7.7, 8.6, 12],
+        [7.8, 8.8, 12.2],
+        [8, 8.9, 12.5],
+        [8.1, 9.1, 12.7],
+        [8.2, 9.2, 12.9],
+        [8.4, 9.4, 13.2],
+        [8.5, 9.5, 13.4],
+        [8.6, 9.7, 13.6],
+        [8.8, 9.8, 13.9],
+        [8.9, 10, 14.1],
+        [9, 10.1, 14.3],
+        [9.1, 10.2, 14.5],
+        [9.2, 10.4, 14.8],
+        [9.4, 10.5, 15],
+        [9.5, 10.7, 15.2],
+        [9.6, 10.8, 15.4],
+        [9.7, 10.9, 15.6],
+        [9.8, 11, 15.8],
+        [9.9, 11.2, 16],
+        [10, 11.3, 16.2],
+        [10.1, 11.4, 16.4],
+        [10.2, 11.5, 16.6],
+        [10.3, 11.6, 16.8],
+        [10.4, 11.8, 17],
+        [10.5, 11.9, 17.2],
+        [10.6, 12, 17.4],
+        [10.7, 12.1, 17.6],
+        [10.8, 12.2, 17.8],
+        [10.9, 12.4, 18],
+        [11, 12.5, 18.2],
+        [11.1, 12.6, 18.4],
+        [11.2, 12.7, 18.6],
+        [11.3, 12.8, 18.8],
+        [11.4, 12.9, 19],
+        [11.5, 13.1, 19.2],
+        [11.6, 13.2, 19.4],
+        [11.7, 13.3, 19.6],
+        [11.8, 13.4, 19.8],
+        [11.9, 13.5, 20],
+        [12, 13.6, 20.2],
+        [12.1, 13.7, 20.4],
+        [12.2, 13.8, 20.6],
+        [12.3, 14, 20.8],
+        [12.4, 14.1, 21]
+      ];
+      if (_y < dataList[_x][0]) {
+        setState(() {
+          _warna = Colors.red;
+        });
+        _hasil = "Berat badan sangat kurang (severely underweight)";
+        _keterangan = "Meningkatkan kualitas makanan bukan jumlah "
+            "makanan, berikan nutrisi yang mengandung zat besi "
+            "dan protein. Penuhi kebutuhan susu anakan sekitar "
+            "350-400 ml per hari. Batasi juga asupan cairan untuk anak,"
+            "terlalu banyak minum bisa menyebabkan diare dan menurunkan "
+            "nafsu makan.";
+      } else if (_y >= dataList[_x][0] && _y < dataList[_x][1]) {
+        setState(() {
+          _warna = Colors.orangeAccent;
+        });
+        _hasil = "Berat badan kurang (underweight)";
+        _keterangan = "Meningkatkan pemberian ASI untuk bayi Anda "
+            "terutama yang dibawah 6 bulan. Pemberian MPASI apabila usia "
+            "bayi sudah 6 bulan ke atas. Fokus pada kualitas makanan, "
+            "berikan nutrisi yang mengandung zat besi dan protein. ";
+      } else if (_y >= dataList[_x][1] && _y < dataList[_x][2]) {
+        setState(() {
+          _warna = Colors.green;
+        });
+        _hasil = "Berat badan normal";
+        _keterangan = "Bagus! Bayi anda telah memperoleh nutrisi yang cukup. "
+            "Untuk memantau pertumbuhan si kecil, jangan lupa untuk "
+            "rutin menimbang berat badannya setiap bulan di puskesmas atau "
+            "posyandu dan konsultasikan tumbuh kembang anak. Pertahankan "
+            "asupan nutrisi yang baik dan pola makan bayi yang teratur. ";
+      } else {
+        setState(() {
+          _warna = Colors.redAccent;
+        });
+        _hasil = "Risiko Berat badan lebih";
+        _keterangan = "Berikan bayi frekuensi serta porsi makan yang tepat "
+            "sesuai dengan usianya. Jika nantinya asupan kalori harian "
+            "bayi perlu dikurangi, biasanya dokter atau ahli gizi akan "
+            "membantu merencanakannya dengan baik.";
+      }
     } else {
-      setState(() {
-                 _warna=Colors.redAccent;
-                }
-  );
-      _hasil = "Risiko Berat badan lebih";
-    }
-    }else{
       var dataList = [
-      [2,2.4,3.7],
-[2.7,3.2,4.8],
-[3.4,3.9,5.8],
-[4,4.5,6.6],
-[4.4,5,7.3],
-[4.8,5.4,7.8],
-[5.1,5.7,8.2],
-[5.3,6,8.6],
-[5.6,6.3,9],
-[5.8,6.5,9.3],
-[5.9,6.7,9.6],
-[6.1,6.9,9.9],
-[6.3,7,10.1],
-[6.4,7.2,10.4],
-[6.6,7.4,10.6],
-[6.7,7.6,10.9],
-[6.9,7.7,11.1],
-[7,7.9,11.4],
-[7.2,8.1,11.6],
-[7.3,8.2,11.8],
-[7.5,8.4,12.1],
-[7.6,8.6,12.3],
-[7.8,8.7,12.5],
-[7.9,8.9,12.8],
-[8.1,9,13],
-[8.2,9.2,13.3],
-[8.4,9.4,13.5],
-[8.5,9.5,13.7],
-[8.6,9.7,14],
-[8.8,9.8,14.2],
-[8.9,10,14.4],
-[9,10.1,14.7],
-[9.1,10.3,14.9],
-[9.3,10.4,15.1],
-[9.4,10.5,15.4],
-[9.5,10.7,15.6],
-[9.6,10.8,15.8],
-[9.7,10.9,16],
-[9.8,11.1,16.3],
-[9.9,11.2,16.5],
-[10.1,11.3,16.7],
-[10.2,11.5,16.9],
-[10.3,11.6,17.2],
-[10.4,11.7,17.4],
-[10.5,11.8,17.6],
-[10.6,12,17.8],
-[10.7,12.1,18.1],
-[10.8,12.2,18.3],
-[10.9,12.3,18.5],
-[11,12.4,18.8],
-[11.1,12.6,19],
-[11.2,12.7,19.2],
-[11.3,12.8,19.4],
-[11.4,12.9,19.7],
-[11.5,13,19.9],
-[11.6,13.2,20.1],
-[11.7,13.3,20.3],
-[11.8,13.4,20.6],
-[11.9,13.5,20.8],
-[12,13.6,21],
-[12.1,13.7,21.2],
-    ];
-    if (_y < dataList[_x][0]) {
-      setState(() {
-                 _warna=Colors.red;
-                }
-  );
-      _hasil = "Berat badan sangat kurang (severely underweight)";
-    } else if (_y >= dataList[_x][0] && _y < dataList[_x][1]) {
-      setState(() {
-                 _warna=Colors.orangeAccent;
-                }
-  );
-      _hasil = "Berat badan kurang (underweight)";
-    } else if (_y >= dataList[_x][1] && _y < dataList[_x][2]) {
-      setState(() {
-                 _warna=Colors.green;
-                }
-  );
-      _hasil = "Berat badan normal";
-    } else {
-      setState(() {
-                 _warna=Colors.redAccent;
-                }
-  );
-      _hasil = "Risiko Berat badan lebih";
-    }
+        [2, 2.4, 3.7],
+        [2.7, 3.2, 4.8],
+        [3.4, 3.9, 5.8],
+        [4, 4.5, 6.6],
+        [4.4, 5, 7.3],
+        [4.8, 5.4, 7.8],
+        [5.1, 5.7, 8.2],
+        [5.3, 6, 8.6],
+        [5.6, 6.3, 9],
+        [5.8, 6.5, 9.3],
+        [5.9, 6.7, 9.6],
+        [6.1, 6.9, 9.9],
+        [6.3, 7, 10.1],
+        [6.4, 7.2, 10.4],
+        [6.6, 7.4, 10.6],
+        [6.7, 7.6, 10.9],
+        [6.9, 7.7, 11.1],
+        [7, 7.9, 11.4],
+        [7.2, 8.1, 11.6],
+        [7.3, 8.2, 11.8],
+        [7.5, 8.4, 12.1],
+        [7.6, 8.6, 12.3],
+        [7.8, 8.7, 12.5],
+        [7.9, 8.9, 12.8],
+        [8.1, 9, 13],
+        [8.2, 9.2, 13.3],
+        [8.4, 9.4, 13.5],
+        [8.5, 9.5, 13.7],
+        [8.6, 9.7, 14],
+        [8.8, 9.8, 14.2],
+        [8.9, 10, 14.4],
+        [9, 10.1, 14.7],
+        [9.1, 10.3, 14.9],
+        [9.3, 10.4, 15.1],
+        [9.4, 10.5, 15.4],
+        [9.5, 10.7, 15.6],
+        [9.6, 10.8, 15.8],
+        [9.7, 10.9, 16],
+        [9.8, 11.1, 16.3],
+        [9.9, 11.2, 16.5],
+        [10.1, 11.3, 16.7],
+        [10.2, 11.5, 16.9],
+        [10.3, 11.6, 17.2],
+        [10.4, 11.7, 17.4],
+        [10.5, 11.8, 17.6],
+        [10.6, 12, 17.8],
+        [10.7, 12.1, 18.1],
+        [10.8, 12.2, 18.3],
+        [10.9, 12.3, 18.5],
+        [11, 12.4, 18.8],
+        [11.1, 12.6, 19],
+        [11.2, 12.7, 19.2],
+        [11.3, 12.8, 19.4],
+        [11.4, 12.9, 19.7],
+        [11.5, 13, 19.9],
+        [11.6, 13.2, 20.1],
+        [11.7, 13.3, 20.3],
+        [11.8, 13.4, 20.6],
+        [11.9, 13.5, 20.8],
+        [12, 13.6, 21],
+        [12.1, 13.7, 21.2],
+      ];
+      if (_y < dataList[_x][0]) {
+        setState(() {
+          _warna = Colors.red;
+        });
+        _hasil = "Berat badan sangat kurang (severely underweight)";
+        _keterangan = "Meningkatkan kualitas makanan bukan jumlah "
+            "makanan, berikan nutrisi yang mengandung zat besi "
+            "dan protein. Penuhi kebutuhan susu anakan sekitar "
+            "350-400 ml per hari. Batasi juga asupan cairan untuk anak,"
+            "terlalu banyak minum bisa menyebabkan diare dan menurunkan "
+            "nafsu makan.";
+      } else if (_y >= dataList[_x][0] && _y < dataList[_x][1]) {
+        setState(() {
+          _warna = Colors.orangeAccent;
+        });
+        _hasil = "Berat badan kurang (underweight)";
+        _keterangan = "Meningkatkan pemberian ASI untuk bayi Anda "
+            "terutama yang dibawah 6 bulan. Pemberian MPASI apabila usia "
+            "bayi sudah 6 bulan ke atas. Fokus pada kualitas makanan, "
+            "berikan nutrisi yang mengandung zat besi dan protein. ";
+      } else if (_y >= dataList[_x][1] && _y < dataList[_x][2]) {
+        setState(() {
+          _warna = Colors.green;
+        });
+        _hasil = "Berat badan normal";
+        _keterangan = "Bagus! Bayi anda telah memperoleh nutrisi yang cukup. "
+            "Untuk memantau pertumbuhan si kecil, jangan lupa untuk "
+            "rutin menimbang berat badannya setiap bulan di puskesmas atau "
+            "posyandu dan konsultasikan tumbuh kembang anak. Pertahankan "
+            "asupan nutrisi yang baik dan pola makan bayi yang teratur. ";
+      } else {
+        setState(() {
+          _warna = Colors.redAccent;
+        });
+        _hasil = "Risiko Berat badan lebih";
+        _keterangan = "Berikan bayi frekuensi serta porsi makan yang tepat "
+            "sesuai dengan usianya. Jika nantinya asupan kalori harian"
+            "bayi perlu dikurangi, biasanya dokter atau ahli gizi akan "
+            "membantu merencanakannya dengan baik.";
+      }
     }
     return _hasil;
   }
@@ -208,10 +239,38 @@ class _UmurBBState extends State<UmurBB> {
   void _prosesUBB() {
     _hasil = bbController.text;
     double _bb = double.parse(bbController.text);
-    int _xx = _value.toInt();
+    int _xx = _value.toInt(); //nilai bulan
     setState(() {
-      _hasil = _rumus_umur_bb(_xx, _bb,dropdownValue);
+      _hasil = _rumus_umur_bb(_xx, _bb, dropdownValue);
     });
+    //menambah alert bb
+    _bbAlert(context);
+  }
+
+  void _bbAlert(BuildContext context) {
+    Widget okButton = FlatButton(
+      child: Text("Oke"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+    var alertDialog = AlertDialog(
+      title: Text(
+        '$_hasil',
+        style: TextStyle(
+          color: _warna,
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      content: Text('$_keterangan'),
+      actions: [okButton],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+    showDialog(
+        context: context, builder: (BuildContext context) => alertDialog);
   }
 
   Widget form_umur_bb() {
@@ -323,7 +382,7 @@ class _UmurBBState extends State<UmurBB> {
                               setState(
                                 () {
                                   _value = value;
-                                  _valueSlide=value.round();
+                                  _valueSlide = value.round();
                                 },
                               );
                             },
@@ -385,16 +444,16 @@ class _UmurBBState extends State<UmurBB> {
             SizedBox(
               height: 16,
             ),
-             Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: _warna,
-                      borderRadius: BorderRadius.circular(10)
-                    ),
-                    child:
-                  Text('$_hasil',textAlign: TextAlign.center, style: TextStyle(color: Colors.white),)
-                  )
+            Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                    color: _warna, borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  '$_hasil',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ))
           ],
         ))
       ],
@@ -414,10 +473,9 @@ class _UmurBBState extends State<UmurBB> {
           //   )
 
           // ],)
-          SingleChildScrollView(child:
-          Padding(padding: EdgeInsets.all(16),
-            child:form_umur_bb()
-            )),
+          SingleChildScrollView(
+              child:
+                  Padding(padding: EdgeInsets.all(16), child: form_umur_bb())),
     );
   }
 }
